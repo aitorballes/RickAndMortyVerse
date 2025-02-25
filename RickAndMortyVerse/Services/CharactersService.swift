48640b77a5728411211c4be3d3ebdf8598723771
Aitor Ballesteros <aitorballesteros@gmail.com>
2025-02-25 09:07:56 +0100

    Add new method to obtain next characters and refactor code

--- RickAndMortyVerse/Services/CharactersService.swift
+++ RickAndMortyVerse/Services/CharactersService.swift
@@ -1,8 +1,17 @@
 import Foundation
-class CharactersService {
+
+class CharactersService {   
     
-    func getAllCharacters(completion: @escaping (CharactersRequestModel?) -> Void) {
-        guard let url = URL(string:apiBaseUrl + "character") else{
+    func getCharacters(completion: @escaping (CharactersRequestModel?) -> Void) {
+        fetchCharacters(from: "character", completion: completion)
+    }
+    
+    func getCharactersByPage(page: String, completion: @escaping (CharactersRequestModel?) -> Void) {
+        fetchCharacters(from: "character?page=\(page)", completion: completion)
+    }
+    
+    private func fetchCharacters(from endpoint: String, completion: @escaping (CharactersRequestModel?) -> Void) {
+        guard let url = URL(string: apiBaseUrl + endpoint) else {
             completion(nil)
             return
         }
@@ -10,33 +19,30 @@ class CharactersService {
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         
-        
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             let jsonDecoder = JSONDecoder()
-
+            
             if let error = error {
                 print(error)
                 completion(nil)
                 return
             }
+            
             guard let data = data else {
                 print("Data was nil")
                 completion(nil)
                 return
             }
-
+            
             do {
-                let proverbModel = try jsonDecoder.decode(CharactersRequestModel.self, from: data)
-                completion(proverbModel)
+                let charactersModel = try jsonDecoder.decode(CharactersRequestModel.self, from: data)
+                completion(charactersModel)
             } catch {
                 print("Error decoding: \(error)")
                 completion(nil)
             }
         }
-
-        task.resume()
         
+        task.resume()
     }
-    
 }
-
--- RickAndMortyVerse/ViewModels/CharactersViewModel.swift
+++ RickAndMortyVerse/ViewModels/CharactersViewModel.swift
@@ -3,16 +3,42 @@ import Observation
 
 @Observable
 class CharactersViewModel {
-    let charactersService = CharactersService()
+    private let charactersService = CharactersService()
     
-    var characters: [CharacterModel]? = nil
+    var characters: [CharacterModel] = []
+    var nextPage: String?
     
-    func getAllCharacters() {
-        charactersService.getAllCharacters { [weak self] result in
+    func getCharacters() {
+        charactersService.getCharacters { [weak self] result in
             DispatchQueue.main.async {
-                self?.characters = result?.results
+                guard let self = self, let result = result else { return }
+                self.characters = result.results ?? []
+                self.nextPage = result.info?.next
             }
-            
         }
+    }
+    
+    func getCharactersByPage(page: String) {
+        charactersService.getCharactersByPage(page: page) { [weak self] result in
+            DispatchQueue.main.async {
+                guard let self = self, let result = result else { return }
+                self.characters.append(contentsOf: result.results ?? [])
+                self.nextPage = result.info?.next
+            }
+        }
+    }
+    
+    func getNextCharacters() {
+        guard let page = nextPage, let pageNumber = extractPageNumber(from: page) else { return }
+        getCharactersByPage(page: pageNumber)
+    }   
+    
+    private func extractPageNumber(from url: String) -> String? {
+        let pattern = "\\?page=(\\d+)"
+        guard let regex = try? NSRegularExpression(pattern: pattern),
+              let match = regex.firstMatch(in: url, range: NSRange(url.startIndex..., in: url)),
+              let range = Range(match.range(at: 1), in: url) else { return nil }
+        
+        return String(url[range])
     }
 }

e257ace90ffeef5dec97e5b3d58ddfd26cbbfa59
Aitor Ballesteros <aitorballesteros@gmail.com>
2025-02-27 10:44:46 +0100

    Modify viewmodel to use the services methods asynchronous and add filter methods

---
+++ RickAndMortyVerse/Models/CharacterFiltersModel.swift
@@ -0,0 +1,7 @@
+import Foundation
+struct CharacterFiltersModel {
+    let name: String?
+    let gender: Gender?
+    let status: Status?
+    let species: Species?
+}
--- RickAndMortyVerse/ViewModels/CharactersViewModel.swift
+++ RickAndMortyVerse/ViewModels/CharactersViewModel.swift
@@ -4,47 +4,64 @@ import Observation
 @Observable
 class CharactersViewModel {
     private let charactersService: CharactersService
-    
-    init(charactersService: CharactersService = CharactersService()){
+
+    init(charactersService: CharactersService = CharactersService()) {
         self.charactersService = charactersService
     }
-    
+
     var characters: [CharacterModel] = []
-    var nextPage: String? 
+    var nextPage: String?
     var isBusy: Bool = false
-    
-    func getCharacters() {
+    var filters: CharacterFiltersModel?
+
+    @MainActor
+    func getCharacters() async {
         isBusy = true
-        charactersService.getCharacters { [weak self] result in
-            DispatchQueue.main.async {
-                guard let self = self, let result = result else {
-                    self?.isBusy = false
-                    return
-                }
+        do {
+            let result = try await charactersService.getCharacters()
+            self.characters = result.results ?? []
+            self.nextPage = result.info?.next
+            self.isBusy = false
+        } catch {
+            self.isBusy = false
+            print("Error fetching characters: \(error)")
+        }
+    }
+
+    @MainActor
+    func getCharactersByPage(page: String) async {
+        isBusy = true
+        do {
+            let result = try await charactersService.getCharactersByPage(page: page)
+            self.characters.append(contentsOf: result.results ?? [])
+            self.nextPage = result.info?.next
+            self.isBusy = false
+        } catch {
+            self.isBusy = false
+            print("Error fetching characters by page: \(error)")
+        }
+    }
+
+    func getNextCharacters() async {
+        guard let page = nextPage, let pageNumber = page.extractPageNumber() else { return }
+        await getCharactersByPage(page: pageNumber)
+    }
+
+    @MainActor
+    func applyFilters(searchText: String?, gender: Gender?, status: Status?, species: Species?) async {
+        self.filters = .init(
+            name: searchText, gender: gender, status: status, species: species)
+        if let filter = filters {
+            isBusy = true
+            do {
+                let result = try await charactersService.getCharactersByFilters(filter: filter)
                 self.characters = result.results ?? []
                 self.nextPage = result.info?.next
                 self.isBusy = false
-            }
-        }
-    }
-    
-    func getCharactersByPage(page: String) {
-        isBusy = true
-        charactersService.getCharactersByPage(page: page) { [weak self] result in
-            DispatchQueue.main.async {
-                guard let self = self, let result = result else {
-                    self?.isBusy = false
-                    return
-                }
-                self.characters.append(contentsOf: result.results ?? [])
-                self.nextPage = result.info?.next
+            } catch {
                 self.isBusy = false
+                print("Error applying filters: \(error)")
             }
         }
-    }
-    
-    func getNextCharacters() {
-        guard let page = nextPage, let pageNumber = page.extractPageNumber() else { return }
-        getCharactersByPage(page: pageNumber)
     }
 }

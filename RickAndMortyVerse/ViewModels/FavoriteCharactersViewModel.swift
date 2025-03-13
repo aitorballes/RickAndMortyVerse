6ce8c8dc9b38fda2112b8092967771a3365634ae
Aitor Ballesteros <aitorballesteros@gmail.com>
2025-03-13 12:51:23 +0100

    Add favorites character view and view model

---
+++ RickAndMortyVerse/ViewModels/FavoriteCharactersViewModel.swift
@@ -0,0 +1,39 @@
+import Foundation
+import CoreData
+import Observation
+
+@Observable
+class FavoriteCharactersViewModel {
+    var favoriteCharacters: [FavoriteCharacter] = []
+    
+    private let context: NSManagedObjectContext
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+        fetchFavorites()
+    }
+    
+    func fetchFavorites() {
+        let request: NSFetchRequest<FavoriteCharacter> = FavoriteCharacter.fetchRequest()
+        do {
+            favoriteCharacters = try context.fetch(request)
+        } catch {
+            print("Error fetching favorite characters: \(error)")
+        }
+    }
+    
+    func deleteFavorite(at offsets: IndexSet) {
+        for index in offsets {
+            let character = favoriteCharacters[index]
+            context.delete(character)
+        }
+        do {
+            try context.save()
+            fetchFavorites()
+        } catch {
+            print("Error deleting favorite character: \(error)")
+        }
+    }
+    
+    
+}
---
+++ RickAndMortyVerse/Views/FavoriteCharactersView.swift
@@ -0,0 +1,60 @@
+import CoreData
+import SwiftUI
+
+struct FavoriteCharactersView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.dismiss) var dismiss
+    @State var viewModel: FavoriteCharactersViewModel
+
+    init(context: NSManagedObjectContext) {
+        _viewModel = State(initialValue: FavoriteCharactersViewModel(context: context))
+    }
+    
+    var body: some View {
+        
+        VStack
+        {
+            if !viewModel.favoriteCharacters.isEmpty {
+                List {
+                    ForEach(viewModel.favoriteCharacters, id: \.self) { character in
+                        HStack {
+                            CustomImageView(imageUrl: character.image ?? "", width: 60, height: 60, cornerRadius: 12)
+
+                            VStack(alignment: .leading) {
+                                Text(character.name ?? "")
+                                    .font(.body)
+                                    .fontWeight(.semibold)
+                                    .multilineTextAlignment(.leading)
+                                Text("Status: \(character.status ?? "")")
+                                .font(.footnote)
+                                .fontWeight(.light)
+                            }
+                            
+                            Spacer()
+                        }
+                        
+                    }
+                    .onDelete(perform: viewModel.deleteFavorite)
+                }
+            } else {
+                ContentUnavailableView(
+                    "No Favorite Characters Available", systemImage: "star.fill",
+                    description: Text(
+                        "Try adding a new one by searching for a character in the Character List"
+                    ))
+            }
+        }
+        
+        .navigationTitle("Favorite Characters")
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button(action: {
+                    dismiss()
+                }) {
+                    Image(systemName: "chevron.left")
+                }
+            }
+        }
+    }
+}
